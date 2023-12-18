ddc2869459f4514da8007329b84e50249c77b280
Soobin Yang <syang26@wooster.edu>
2023-12-18 21:36:54 +0900

    change s path

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Favicon from "react-favicon";
+import Favicon from 'react-favicon'
+import s from './assets/s.png'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Favicon url="src/assets/s.png" />
+    <Favicon url={s} />
     <App />
   </React.StrictMode>,
 )
